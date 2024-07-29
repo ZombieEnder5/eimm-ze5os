c4f89bce0838721e69852dd8c64e4d377b860515
ZombieEnder5 <zombieender56@gmail.com>
2024-07-29 12:22:20 -0700

    * Release 1.0.0

--- Plugin.cs
+++ Plugin.cs
@@ -3,69 +3,57 @@ using System;
 using System.IO;
 using UnityEngine;
 using Utilla;
-
 namespace eimm_ze5os
 {
-    /// <summary>
-    /// This is your mod's main class.
-    /// </summary>
-
+    // this mod is not intended to affect gameplay
+    // (which I really don't know how THIS mod would be able to affect gameplay)
     [BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")]
     [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
     public class Plugin : BaseUnityPlugin
     {
-        bool inRoom;
-        Vector3 texPos = new Vector3(-66.3869f, 11.9003f, -82.4537f);
-        Texture2D tex;
+        bool initialized = false;
+        bool wasEnabled = false;
+        Vector3 objPos = new Vector3(-69.1193f, 12.6312f, -81.7537f);
+        Vector3 objRot = new Vector3(0f, 289.8361f, 0f);
+        Vector3 objScale = new Vector3(1.5127f, 0.3236f, 1f);
         GameObject obj;
-
+        GameObject copy;
         void Start()
         {
-            Debug.Log("STARTING 'EIMM' MOD");
             Events.GameInitialized += OnGameInitialized;
         }
-
         void OnEnable()
         {
-            /* Set up your mod here */
-            /* Code here runs at the start and whenever your mod is enabled*/
-            obj = GameObject.CreatePrimitive(PrimitiveType.Quad);
-            MeshCollider collider = obj.GetComponent<MeshCollider>();
-            if (collider != null)
-                collider.enabled = false;
-            obj.GetComponent<MeshRenderer>().material.mainTexture = tex;
-            obj.transform.position = texPos;
-            obj.transform.localScale = new Vector3(5, 1, 1);
-            obj.name = "everyone ignored my message";
+            if (initialized)
+                initObj();
+            wasEnabled = true;
             HarmonyPatches.ApplyHarmonyPatches();
         }
-
+        void initObj()
+        {
+            copy = Instantiate(obj, objPos, Quaternion.Euler(objRot));
+            copy.transform.localScale = objScale;
+            copy.name = "everyone ignored my message";
+        }
         void OnDisable()
         {
-            /* Undo mod setup here */
-            /* This provides support for toggling mods with ComputerInterface, please implement it :) */
-            /* Code here runs whenever your mod is disabled (including if it disabled on startup)*/
-            Destroy(obj);
+            if (initialized)
+                Destroy(copy);
+            wasEnabled = false;
             HarmonyPatches.RemoveHarmonyPatches();
         }
-
         void OnGameInitialized(object sender, EventArgs e)
         {
-            /* Code here runs after the game initializes (i.e. GorillaLocomotion.Player.Instance != null) */
-            Debug.Log("init!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
             string appDir = Path.GetDirectoryName(Application.dataPath);
             string modDir = Path.Combine(appDir, "BepInEx", "plugins", "eimm-ze5os");
             string bundlePath = Path.Combine(modDir, "evryignmymsg");
             AssetBundle bundle = AssetBundle.LoadFromFile(bundlePath);
             if (bundle == null)
                 throw new Exception("bundle not found or failed to load");
-            tex = bundle.LoadAsset<Texture2D>("everyone ignored my message");
+            obj = bundle.LoadAsset<GameObject>("thingus");
             bundle.Unload(false);
-        }
-
-        void Update()
-        {
-            /* Code here runs every frame when the mod is enabled */
+            if (wasEnabled)
+                initObj();
         }
     }
 }
